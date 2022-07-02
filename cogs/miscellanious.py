2f551ab533baee2ed5f180b9f08ccef194317267
xDrakiiii <106721270+xDrakiiii@users.noreply.github.com>
2022-07-02 18:47:48 -0400

    Update miscellanious.py

--- cogs/miscellanious.py
+++ cogs/miscellanious.py
@@ -1,6 +1,5 @@
 import discord
 from discord import ApplicationContext, Colour
-import psutil
 from discord.ext import commands
 
 from core import checks
@@ -11,18 +10,6 @@ class Miscellaneous(BaseCog):
     _id="miscellaneous"
     
     default_cache={}
-
-    @commands.slash_command(name="usage", description="Gets Usage")
-    @checks.has_permissions(PermissionLevel.REGULAR)
-    async def usage(self, ctx: ApplicationContext):
-        embed = discord.Embed(title="Stats", colour=Colour.green())
-
-        embed.add_field(name="CPU Usage", value=f"{psutil.cpu_percent()}", inline=False)
-        embed.add_field(name="Memory Usage", value=f"{psutil.virtual_memory().used}/{psutil.virtual_memory().total} ({psutil.virtual_memory().percent}\%)", inline=False)
-        embed.add_field(name="Disk Usage", value=f"{psutil.disk_usage('/').used}/{psutil.disk_usage('/').total} ({psutil.disk_usage('/').percent}\%)", inline=False)
-
-        await ctx.respond(embed=embed)
-
 
     @commands.slash_command(name="idp", description="It depends")
     @checks.has_permissions(PermissionLevel.REGULAR)
